85720b09a3b3cedded28923d3bd82d55eddb1f4c
spayker <spykerstar@gmail.com>
2021-07-26 09:41:53 +0200

    modified GlobalKeyListener.java

--- src/main/java/com/spayker/s2t/sphinx/keyboard/GlobalKeyListener.java
+++ src/main/java/com/spayker/s2t/sphinx/keyboard/GlobalKeyListener.java
@@ -1,40 +1,38 @@
 package com.spayker.s2t.sphinx.keyboard;
 
 import com.spayker.s2t.sphinx.recognition.SoundRecognizer;
-import edu.cmu.sphinx.api.LiveSpeechRecognizer;
-import edu.cmu.sphinx.api.SpeechResult;
 import org.jnativehook.keyboard.NativeKeyEvent;
 import org.jnativehook.keyboard.NativeKeyListener;
 
 public class GlobalKeyListener implements NativeKeyListener {
 
     private SoundRecognizer soundRecognizer;
-    private SpeechResult result;
+
+    private boolean isPressed;
 
     private GlobalKeyListener() { }
 
     public GlobalKeyListener(SoundRecognizer soundRecognizer) {
         this.soundRecognizer = soundRecognizer;
-        soundRecognizer.startResourcesThread();
     }
 
     public void nativeKeyPressed(NativeKeyEvent e) {
-        System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
-
-        if (e.getKeyCode() == NativeKeyEvent.VC_CONTROL_L || e.getKeyCode() == NativeKeyEvent.VC_CONTROL_R) {
+        if (!isPressed && e.getKeyCode() == NativeKeyEvent.VC_CONTROL_L || e.getKeyCode() == NativeKeyEvent.VC_CONTROL_R) {
             System.out.println("Ctrl pressed");
 
             // Start recognition process pruning previously cached data.
             soundRecognizer.startSpeechRecognition();
+            isPressed = true;
         }
     }
 
     public void nativeKeyReleased(NativeKeyEvent e) {
         System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
 
-        if (e.getKeyCode() == NativeKeyEvent.VC_CONTROL_L || e.getKeyCode() == NativeKeyEvent.VC_CONTROL_R) {
+        if (isPressed && e.getKeyCode() == NativeKeyEvent.VC_CONTROL_L || e.getKeyCode() == NativeKeyEvent.VC_CONTROL_R) {
             System.out.println("Ctrl released");
-
+            isPressed = false;
+            soundRecognizer.setSpeechRecognizerThreadRunning(false);
         }
     }
 
